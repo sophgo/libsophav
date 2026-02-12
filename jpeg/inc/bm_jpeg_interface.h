ba5b68fe8df20b040f136d438917565614a649a4
yongle_wan <yongle.wan01@sophgo.com>
2026-02-12 17:01:36 +0800

    Bugfix: jpeg encode Compatibility issue.
    Change-Id: I27c422b36c514aaf69050baed6ca70b0a49d5fe3
    (cherry picked from commit 2273a41b9ce10184fa7eaa889962652d16f235b5)

--- jpeg/inc/bm_jpeg_interface.h
+++ jpeg/inc/bm_jpeg_interface.h
@@ -173,9 +173,6 @@ typedef struct {
     /* DMA buffer which contains the pixels. */
     bm_device_mem_t *dma_buffer;
 
-    bm_device_mem_t *dma_buffer_y; // when dma_buffer is empty, dma_buffer_y/dma_buffer_u/dma_buffer_v will be used.
-    bm_device_mem_t *dma_buffer_u; // nv12: set uv dma_buffer
-    bm_device_mem_t *dma_buffer_v; // nv21: set vu dma_buffer
     /* These define the starting offsets of each component
      * relative to the start of the buffer. Specified in bytes.
      */
@@ -197,6 +194,12 @@ typedef struct {
 
     /* Internal, implementation-defined data. Do not modify. */
     void *internal;
+
+    /* Separate DMA buffers which contains the pixels.
+     * when dma_buffer is empty, dma_buffer_y/dma_buffer_u/dma_buffer_v will be used. */
+    bm_device_mem_t *dma_buffer_y;
+    bm_device_mem_t *dma_buffer_u;
+    bm_device_mem_t *dma_buffer_v;
 } BmJpuFramebuffer;
 
 /* Structure containing details about encoded frames. */
